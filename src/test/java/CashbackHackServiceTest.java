244124e859304fd89e998ec788acb84bc0f5609b
Краснова Татьяна <redtanya@yandex.ru>
2022-08-04 16:17:00 +0300

    delete 1000

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -23,13 +23,5 @@ public class CashbackHackServiceTest {
 
     }
 
-    @Test
-    public void shouldAmount1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int expected = 0;
-        int actual = service.remain(1000);
-        Assert.assertEquals(actual, expected);
 
-    }
 }
